e0c594c143f2cddffd7a7f222a75239053883c1f
PedroHMellow <143191198+PedroHMellow@users.noreply.github.com>
2025-09-02 18:13:08 -0300

    Add files via upload

---
+++ Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static double CalcularAreaCirculo(double raio)
+        {
+            return Math.PI * Math.Pow(raio, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o raio do círculo: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalcularAreaCirculo(raio);
+
+            Console.WriteLine($"{nome}, a área do círculo é: {area:F2}");
+        }
+    }
+}
